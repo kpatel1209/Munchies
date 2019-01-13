9ce2d6d81ea6d6ccc8577be8c5b49e3179ed5c32
Kevin Patel <kpatel1209@gmail.com>
2019-01-13 16:46:47 -0500

    added favorite heart icon on recipe box, not complete yet; added some popular ingredients in an array so the auto-complete function can be utilized in the user input ingredient section

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -5,6 +5,10 @@ $(document).ready(function(){
     // $("#second-column").hide();
     // $("#third-column").hide();
 
+    // List of popular ingredients to feed the auto-complete function
+    $('input.autocomplete').autocomplete({
+        data: {"Apple": null, "Banana": null, "Carrot": null, "Cheese": null, "Bread": null, "Eggs": null, "Milk": null, "Onions": null, "Beans": null, "Rice": null, "Pork": null, "Beef": null,"Bacon": null,"Potatoes": null, "Butter": null,"Mushrooms": null, "Oil": null, "Lemons": null, "Noodles": null, "Chocolate": null, "Sausage": null, "Flour": null, "Vinegar": null, "Yogurt": null, "Peanut": null, }
+    });
 
 
     $("#submit-ing-btn").on("click", function(event){
