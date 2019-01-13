6f51079cb02099f58fe2c5d3eefa7a1bc9219036
Kevin Patel <kpatel1209@gmail.com>
2019-01-13 15:24:06 -0500

    updated third column with tags for ingredients and nutrition facts, just need to test with API data

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,4 +2,17 @@ console.log("Hello");
 
 $(document).ready(function(){
     $('.sidenav').sidenav();
+    // $("#second-column").hide();
+    // $("#third-column").hide();
+
+
+
+    $("#submit-ing-btn").on("click", function(event){
+        event.preventDefault();
+        // $("#second-column").show();
+        // $("#third-column").show();
+    });
+
+
+
 });
