4c7cc117fc9e1393df6712c76387852e8326b884
Kevin Patel <kpatel1209@gmail.com>
2019-01-14 13:35:39 -0500

    updated the remove button function so now the last entry will removed after click

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -9,7 +9,7 @@ $(document).ready(function(){
 
     // List of popular ingredients to feed the auto-complete function under the Enter Your Ingredients section.
         
-    $('input.autocomplete').autocomplete({
+    $("input.autocomplete").autocomplete({
         data: {"Apple": null, "Banana": null, "Carrot": null, "Cheese": null, "Bread": null, "Eggs": null, "Milk": null, "Onions": null, "Beans": null, "Rice": null, "Pork": null, "Beef": null,"Bacon": null,"Potatoes": null, "Butter": null,"Mushrooms": null, "Oil": null, "Lemons": null, "Noodles": null, "Chocolate": null, "Sausage": null, "Flour": null, "Vinegar": null, "Yogurt": null, "Peanut": null, "Cabbage": null, "Avocado": null, "Ham": null, "Steak": null, "Eggplant": null, "Grapes": null, "Acai": null, "Apricot": null, "Blackberry": null, "Blueberries": null, "Cucumber": null, "Cocounut": null, "Guava": null, "Kiwi": null, "Lime": null, "Mango": null, "Melon": null, "Orange": null, "Papaya": null, "Peach": null, "Pear": null, "Plum": null, "Pineapple": null, "Pomegranate": null, "Rasberries": null, "Stawberries": null, "Olive": null, "Tomatoes": null, "Zucchini": null, "Corn": null, "Broccoli": null, "Lettuce": null, "Celery": null, "Kale": null, "Cauliflower": null, "Asparagus": null, "Radish": null, "Garlic": null, "Okra": null, "Pepper": null, "Spinach": null, "Beet": null, "Yams": null, "Duck": null, "Venison": null, "Lamb": null, "Goat": null, "Turkey": null, "Veal": null, "Ham": null, "Bologne": null, "Lasagne": null, "Spaghetti": null, "Macaroni": null, "Ravioli": null, "Rigatoni": null, "Linguine": null, "Tortellini": null, "Cereal": null, "Penne": null, "Orzo": null, "Margarine": null}
     });
 
@@ -27,18 +27,18 @@ $(document).ready(function(){
         //Check max number of input fields
         if(x < maxField) {
             x++; //Incrememnt field counter
-            $(ingInputLine).append(newIngLine);
+            $(ingInputLine).prepend(newIngLine);
             // Autocomplete function has to be ran for each new field that is added dynamically.         
-            $('input.autocomplete').autocomplete({
+            $("input.autocomplete").autocomplete({
                 data: {"Apple": null, "Banana": null, "Carrot": null, "Cheese": null, "Bread": null, "Eggs": null, "Milk": null, "Onions": null, "Beans": null, "Rice": null, "Pork": null, "Beef": null,"Bacon": null,"Potatoes": null, "Butter": null,"Mushrooms": null, "Oil": null, "Lemons": null, "Noodles": null, "Chocolate": null, "Sausage": null, "Flour": null, "Vinegar": null, "Yogurt": null, "Peanut": null, "Cabbage": null, "Avocado": null, "Ham": null, "Steak": null, "Eggplant": null, "Grapes": null, "Acai": null, "Apricot": null, "Blackberry": null, "Blueberries": null, "Cucumber": null, "Cocounut": null, "Guava": null, "Kiwi": null, "Lime": null, "Mango": null, "Melon": null, "Orange": null, "Papaya": null, "Peach": null, "Pear": null, "Plum": null, "Pineapple": null, "Pomegranate": null, "Rasberries": null, "Stawberries": null, "Olive": null, "Tomatoes": null, "Zucchini": null, "Corn": null, "Broccoli": null, "Lettuce": null, "Celery": null, "Kale": null, "Cauliflower": null, "Asparagus": null, "Radish": null, "Garlic": null, "Okra": null, "Pepper": null, "Spinach": null, "Beet": null, "Yams": null, "Duck": null, "Venison": null, "Lamb": null, "Goat": null, "Turkey": null, "Veal": null, "Ham": null, "Bologne": null, "Lasagne": null, "Spaghetti": null, "Macaroni": null, "Ravioli": null, "Rigatoni": null, "Linguine": null, "Tortellini": null, "Cereal": null, "Penne": null, "Orzo": null, "Margarine": null}
             });
         }
     });
-    // Function for REMOVE ingredient button which allows user to remove lines that they originally added.  Current code removes the very first line, need to figure out how to remove last line instead.
+    // Function for REMOVE ingredient button which allows user to remove lines that they originally added.  Since the ADD function is prepending a new line, the REMOVE function will now remove the most recent line added.
     $(removeIngButton).click(function(event){
         event.preventDefault();
+        $("#new-ingredient").remove();
         x--;
-        $("#new-ingredient:last").remove();
         console.log(removeIngButton);
     });
     
