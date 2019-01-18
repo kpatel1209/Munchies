ce2cb275ffc5aa7288727fc83e8e9d24e8eee2ba
Gunslinger <jtgeislinger@gmail.com>
2019-01-17 19:28:40 -0500

    js update

--- assets/javascript/ajax.js
+++ assets/javascript/ajax.js
@@ -1,34 +1,94 @@
 $(document).ready(function() {
+
+    let ingredientList = [];
+
+    // $("#add-ing-btn").on("change", function(){
+    //     let ingred = $("#user-ing-input").val(this.value);
+    //     // let ingredientList = [];
+    //     // ingredientList.push(ingred);
+    //     console.log(ingred);
+    // });
+    
+    $("#submit-ing-btn").on("click", function(){
+        
+        let searchResult = ["beef", "onion"];
+
+        console.log(searchResult);
+
+        // Clear local storage for this search results
+        localStorage.clear();
+
+        let queryURL = "https://api.edamam.com/search?q=" + searchResult + "&app_id=6d5a3956&app_key=7fec86da650323ec5cbd0d0c2ed5e986";
+
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        })
+        .then(function(response) {
+            // console.log(queryURL);
+            // console.log(response);
+    
+            let results = response.hits;
+    
+            for (let i = 0; i < results.length; i++) {
+                let name = results[i].recipe.label;
+                let img = results[i].recipe.image;
+                let diet = results[i].recipe.dietLabels;
+                let prep = results[i].recipe.totalTime;
+                let servings = results[i].recipe.yield;
+                let ingredients = results[i].recipe.ingredientLines;
+    
+                // Console log each of the API call results
+                console.log("Recipe Name: ", name);
+                console.log("Image Link: ", img);
+                console.log("Diet Type: ", diet);
+                console.log("Prep Time: ", prep);
+                console.log("Servings: ", servings);
+                
+                // Store all the required results into local storage
+                localStorage.setItem("name", JSON.stringify(name));
+                localStorage.setItem("image", JSON.stringify(img));
+                localStorage.setItem("diet", JSON.stringify(diet));
+                localStorage.setItem("prep", JSON.stringify(prep));
+                localStorage.setItem("servings", JSON.stringify(servings));
+                localStorage.setItem("ingredients", JSON.stringify(ingredients));
+    
+                // Add the HTML for the recipe card to the DOM with the results from the API call
+                let recipe = $(`
+                    <div class="row">
+                        <div class="col s12">
+                            <div class="card" id="recipe-boxes">
+                                <div class="card-content" id="recipe-content">
+                                    <div class="recipe-title">
+                                        <h6 class="lable center" id="recipe-name"> 
+                                            <strong>${name}</strong>
+                                            <i class="material-icons" id="fav-icon">favorite_border</i>
+                                        </h6>
+                                    </div>                                        
+                                    <div class="recipe-text">
+                                        <p class="prep">Prep Time: ${prep}</p>
+                                        <p class="diet">Diet: ${diet}</p>
+                                        <p class="yield">Servings: ${servings}</p>
+                                    </div>
+                                    <div>    
+                                        <img src="${img}" id="recipe-image">
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                `);
+    
+                $("#recipe-loadin").append(recipe);
+            }
+        })
+    });
+
+    $("#recipe-boxes").on("click", function() {
+
+    })
     
     
-    let searchResult = "beef";
-
-    let queryURL = "https://api.edamam.com/search?q=" + searchResult + "&app_id=6d5a3956&app_key=7fec86da650323ec5cbd0d0c2ed5e986";
-
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-    })
-    .then(function(response) {
-        // console.log(queryURL);
-        // console.log(response);
-
-        let results = response.hits;
-
-        for (let i = 0; i < results.length; i++) {
-            let name = results[i].recipe.label;
-            let img = results[i].recipe.image;
-            let diet = results[i].recipe.dietLabels;
-            let prep = results[i].recipe.totalTime;
-            let servings = results[i].recipe.yield;
-
-            console.log("Recipe Name: ",  name);
-            console.log("Image Link: ", img);
-            console.log("Diet Type: ", diet);
-            console.log("Prep Time: ", prep);
-            console.log("Servings: ", servings);
-        }
-    })
 })
 
 
--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -16,17 +16,19 @@ $(document).ready(function(){
     let maxField = 10; //Input fields increment limitation
     let addIngButton = $("#add-ing-btn"); //Add Ingredient button selector
     let removeIngButton = $("#remove-ing-btn"); //Remove Ingredient button selector
-    let ingInputLine = $("#user_ing-input"); //Input field
-    let newIngLine = '<input type="text" class="autocomplete" id="new-ingredient" placeholder="(Entry Required)" style="height: 2rem; font-size: 13px; color: white;">'; //New input field
+    let ingInputLine = $("#user-ing-input"); //Input field
+    let newIngLine = '<p id="new-ingredient" style="height: 2rem; font-size: 13px; color: white;"></p>'; //New input field
     let x = 1; //Initial field counter is 1
 
     // Function for ADD ingredient button which allows user to add lines to enter additional ingredients.
     $(addIngButton).click(function(event){
         event.preventDefault();
+        let food = addIngButton.val(this.value);
+        console.log(food);
         //Check max number of input fields
         if(x < maxField) {
             x++; //Incrememnt field counter
-            $(ingInputLine).prepend(newIngLine);
+            $("#new-ingredient").prepend(newIngLine);
             // Autocomplete function has to be ran for each new field that is added dynamically.         
             $("input.autocomplete").autocomplete({
                 data: {"Apple": null, "Banana": null, "Carrot": null, "Cheese": null, "Bread": null, "Eggs": null, "Milk": null, "Onions": null, "Beans": null, "Rice": null, "Pork": null, "Beef": null,"Bacon": null,"Potatoes": null, "Butter": null,"Mushrooms": null, "Oil": null, "Lemons": null, "Noodles": null, "Chocolate": null, "Sausage": null, "Flour": null, "Vinegar": null, "Yogurt": null, "Peanut": null, "Cabbage": null, "Avocado": null, "Ham": null, "Steak": null, "Eggplant": null, "Grapes": null, "Acai": null, "Apricot": null, "Blackberry": null, "Blueberries": null, "Cucumber": null, "Cocounut": null, "Guava": null, "Kiwi": null, "Lime": null, "Mango": null, "Melon": null, "Orange": null, "Papaya": null, "Peach": null, "Pear": null, "Plum": null, "Pineapple": null, "Pomegranate": null, "Rasberries": null, "Stawberries": null, "Olive": null, "Tomatoes": null, "Zucchini": null, "Corn": null, "Broccoli": null, "Lettuce": null, "Celery": null, "Kale": null, "Cauliflower": null, "Asparagus": null, "Radish": null, "Garlic": null, "Okra": null, "Pepper": null, "Spinach": null, "Beet": null, "Yams": null, "Duck": null, "Venison": null, "Lamb": null, "Goat": null, "Turkey": null, "Veal": null, "Ham": null, "Bologne": null, "Lasagne": null, "Spaghetti": null, "Macaroni": null, "Ravioli": null, "Rigatoni": null, "Linguine": null, "Tortellini": null, "Cereal": null, "Penne": null, "Orzo": null, "Margarine": null}
